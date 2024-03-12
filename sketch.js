3ca524354008cc59f75348c702ca1978725cd540
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-03-12 17:48:23 -0400

    syncing

--- sketch.js
+++ sketch.js
@@ -1,14 +1,79 @@
 let playerX = 20;
 let playerY = 25;
-/* >, v, <, ^ */ 
+/* possible values: >, v, <, ^ */ 
 let playerChar = '>';
 const movementSpeed = 5;
 let grid = [];
+const GRID_ROWS = 10;
+const GRID_COLUMNS = 10;
+let interval;
 
 
-function setup() {
-    createCanvas(500, 500);
-    textSize(20);
+function boundByMax(num, max) {
+    return (num > max) ? max : num ;
+}
+
+
+function boundByMin(num, min) {
+    return (num < min) ? min : num ; 
+}
+
+
+function generateMazePath() {
+
+    const DIRECTIONS = ["NORTH",     /* (x, y) -> (x, y - 1)     */
+                        "NORTHEAST", /* (x, y) -> (x + 1, y - 1) */
+                        "EAST",      /* (x, y) -> (x + 1, y)     */
+                        "SOUTHEAST", /* (x, y) -> (x + 1, y + 1) */
+                        "SOUTH",     /* (x, y) -> (x, y + 1)     */
+                        "SOUTHWEST", /* (x, y) -> (x - 1, y + 1) */
+                        "WEST",      /* (x, y) -> (x - 1, y)     */
+                        "NORTHWEST"  /* (x, y) -> (x - 1, y - 1)  */
+    ];
+
+    /** Maze Generating Algorithm (not good) */
+    /*
+        1. Start from a random point on the left side of the maze
+        2. Generate a valid path starting from the left side of the
+            maze until the other side is reached
+    */
+
+    let mazePosX = 0;
+    let mazePosY = 0;
+    console.log(`Maze Positions: (${mazePosX}, ${mazePosY})`);
+
+    mazePosY = random(10);
+
+    while (mazePosX != (GRID_COLUMNS - 1)) {
+        let direction = random(DIRECTIONS);
+
+        if (direction == "NORTH") {
+            mazePosY = boundByMin(mazePosY - 1, 0);
+        } else if (direction == "NORTHEAST") {
+            mazePosY = boundByMin(mazePosY - 1, 0);
+            mazePosX = boundByMax(mazePosX + 1, GRID_COLUMNS);
+        } else if (direction == "EAST") {
+            mazePosX = boundByMax(mazePosX + 1, GRID_COLUMNS);
+        } else if (direction == "SOUTHEAST") {
+            mazePosX = boundByMax(mazePosX + 1, GRID_COLUMNS);
+            mazePosY = boundByMax(mazePosY + 1, GRID_ROWS);
+        } else if (direction == "SOUTH") {
+            mazePosY = boundByMax(mazePosY + 1, GRID_COLUMNS);
+        } else if (direction == "SOUTHWEST") {
+            mazePosY = boundByMax(mazePosY + 1, GRID_ROWS);
+            mazePosX = boundByMin(mazePosX - 1, 0);
+        } else if (direction == "WEST") {
+            mazePosX = boundByMin(mazePosX - 1, 0);
+        } else if (direction == "NORTHWEST") {
+            mazePosX = boundByMin(mazePosX - 1, 0);
+            mazePosY = boundByMin(mazePosY - 1, 0);
+        }
+    
+        grid[mazePosX][mazePosY] = 1;
+    }
+
+    // playerY = mazePosY * (interval - 10);
+    // console.log(`playerY position set to ${playerX}`);
 }
 
 
@@ -25,26 +90,32 @@ function initializeGrid() {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
-
-    const ROWS = 10;
-    const COLUMNS = 20;
-    const DIRECTIONS = ["NORTH", "NORTHEAST", "EAST", "SOUTHEAST",
-                        "SOUTH", "SOUTHWEST", "WEST", "NORTHWEST"];
-
-    /** Maze Generating Algorithm (not good) */
-    /*
-        1. Start from a random point on the left side of the maze
-        2. Generate a valid path starting from the left side of the
-            maze until the other side is reached
-    */
-
-    const startingPoint = random(10);
     
 
 }
 
 
+function setup() {
+    createCanvas(500, 500);
+    textSize(20);
+
+    initializeGrid();
+    generateMazePath();
+    interval = ((width + height) / 2) / 10;
+}
+
+
 function drawMazePath() {
+    fill(255);
+
+    for (let i = 0; i < GRID_ROWS; ++i) {
+        for (let j = 0; j < GRID_COLUMNS; ++j) {
+            if (grid[i][j] == 1) {
+            
+                rect(i * interval, j * interval, interval, interval);
+            }
+        }
+    }
 
 }
 
@@ -55,7 +126,7 @@ function checkMazeCollision() {
 
 
 function drawGrid() {
-    let interval = ((width + height) / 2) / 10;
+    stroke(150);
 
     /* creating columns */
     for (let i = 0; i < width; i += interval) {
@@ -81,6 +152,7 @@ function drawPlayer() {
         playerY += movementSpeed;
     }
 
+    fill(0, 100, 0);
     text(playerChar, playerX, playerY);
 }
 
@@ -103,11 +175,13 @@ function checkBorderCollision() {
 
 
 function draw() {
-    background(255);
-    
+    /** Initialization */
+    background(0);
+
     /** Main Code */
-    drawGrid();
+    drawMazePath();
     drawPlayer();
+    drawGrid();
     checkBorderCollision();
   
     /** Debugging */
